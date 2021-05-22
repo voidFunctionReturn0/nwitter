d61344e2d2ba4403aa9a22e52715ef0e463bb504
minsoo Kim <voidfunctionreturn0@gmail.com>
2021-05-22 13:45:40 +0900

    Setup2

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBGMuUfm7B2MbfU0zx19lGuGx6UqAc-ajc",
+    authDomain: "nwitter-8731b.firebaseapp.com",
+    projectId: "nwitter-8731b",
+    storageBucket: "nwitter-8731b.appspot.com",
+    messagingSenderId: "1030708132205",
+    appId: "1:1030708132205:web:0c560fd19ce353cacec264"
+  };
+
+  firebase.initializeApp(firebaseConfig);
